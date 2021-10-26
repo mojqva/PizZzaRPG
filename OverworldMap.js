48b957a90ef2bae7469ebab81ef5af08b4b3590c
mojqva <majanka@i.ua>
2021-10-26 15:01:26 +0300

    animation added

--- OverworldMap.js
+++ OverworldMap.js
@@ -29,8 +29,8 @@ window.OverworldMaps = {
                 y: utils.withGrid(6),
             }),
             npc1: new Person({
-                x: utils.withGrid(7),
-                y: utils.withGrid(9),
+                x: utils.withGrid(3),
+                y: utils.withGrid(4),
                 src: "/images/characters/people/npc1.png",
             })
         }
